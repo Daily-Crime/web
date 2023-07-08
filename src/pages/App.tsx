7567a2355ec7cd0af07aa811f649f537db6eba63
Arcyvilk <blazemakerrr@gmail.com>
2023-07-08 22:32:28 +0200

    refactor: 💡 try to generate preview

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,15 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { Home } from 'pages/Home';
-import { NotFound } from 'pages/NotFound';
 
 export const App = (): JSX.Element => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/404" element={<NotFound />} />
-        <Route element={'/404'} />
+        <Route element={'/'} />
       </Routes>
     </BrowserRouter>
   );
